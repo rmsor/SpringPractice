df427e69e3c7ae6a9180aca70fd386450554d8e8
rmsor <rmsor.pth02@gmail.com>
2015-04-29 23:44:45 -0500

    annotation basedd config

---
+++ HelloSpring/src/annotations/HelloWorldConfig.java
@@ -0,0 +1,15 @@
+package annotations;
+
+import intro.HelloWorld;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfig {
+
+   @Bean 
+   public HelloWorld helloWorld(){
+      return new HelloWorld();
+   }
+}
